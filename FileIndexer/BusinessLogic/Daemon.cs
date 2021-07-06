de47b42f23888083f3401f7571ce3db6618e8f08
Sven Fehler <svenfehler@sv443.net>
2021-07-06 12:24:45 +0200

    add daemon class

---
+++ FileIndexer/BusinessLogic/Daemon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Daemon
+    {
+        private string path;
+        private FileSystemWatcher watcher;
+
+        public event EventHandler<FileInfo[]> onChange;
+
+        public Daemon(string path, bool recursive = true)
+        {
+            this.path = path;
+
+            watcher = new FileSystemWatcher(this.path);
+            watcher.IncludeSubdirectories = recursive;
+            watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName;
+            watcher.Changed += OnFilesChanged;
+        }
+
+        private void OnFilesChanged(object sender, FileSystemEventArgs e)
+        {
+            FileInfo[] changedFilesList = { };
+
+            // TODO
+
+            onChange?.Invoke(this, changedFilesList);
+
+            /*
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            Console.WriteLine($"Changed: {e.FullPath}");
+            */
+        }
+    }
+}
